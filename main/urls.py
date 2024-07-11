f48ca61d367eb9f595098d771a3e2f64c70a70ba
Yury Petrov <gderpov@yandex.ru>
2024-07-11 21:34:23 +0500

    Добавлено второстепенное навигационное меню. [base_2.html; styles_2.css]

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path, re_path, register_converter
 from . import views 
-from . import converters
 
-register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("about/", views.about, name="about"),
 ]
--- main/views.py
+++ main/views.py
@@ -10,6 +10,10 @@ def index(request):
     return render(request, "main/index.html")
 
 
+def about(request):
+    return render(request, "main/about.html")
+
+
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
