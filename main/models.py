85d6642c9512e4058d4b4cf1c4d62a3aec6d0bed
Yury Petrov <gderpov@yandex.ru>
2024-08-09 19:24:21 +0500

    Добавлена админ-панель

--- main/models.py
+++ main/models.py
@@ -5,8 +5,12 @@ from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, db_index=True)
-    slug = models.SlugField(max_length=255, db_index=True, unique=True)
+    name = models.CharField(max_length=255, db_index=True, verbose_name="Категория")
+    slug = models.SlugField(max_length=255, db_index=True, unique=True, verbose_name="Поле Slug")
+
+    class Meta:
+        verbose_name = "Категории"
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.name
@@ -26,17 +30,19 @@ class Article(models.Model):
         DRAFT = 0, "Черновик"
         PUBLISHED = 1, "Опубликовано"
 
-    title = models.CharField(max_length=300)
-    content = models.TextField()
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT) # type: ignore
-    category = models.ForeignKey("Category", on_delete=models.PROTECT, related_name="articles")
+    title = models.CharField(max_length=300, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Содержание статьи")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Дата последнего обновления")
+    is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)), default=Status.DRAFT, verbose_name="Публикация") # type: ignore
+    category = models.ForeignKey("Category", on_delete=models.PROTECT, related_name="articles", verbose_name="Категория")
 
     objects = models.Manager()
     published = PublishedManager()
 
     class Meta:
+        verbose_name = "Статьи"
+        verbose_name_plural = "Статьи"
         ordering = ["-time_create"]
         indexes = [
             models.Index(fields=["-time_create"])
