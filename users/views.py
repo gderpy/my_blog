3426c668c448fa70a65b95fadc1a4e0bebd2c6ef
Yury Petrov <gderpov@yandex.ru>
2024-08-11 22:33:07 +0500

    Добавлена первоначальная регистрация пользователя [Реализовано через функция представления]

--- users/views.py
+++ users/views.py
@@ -3,28 +3,8 @@ from django.urls import reverse, reverse_lazy
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
 
-# Create your views here.
-
-# def login_user(request):
-#     if request.method == "POST":
-#         form = LoginUserForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(request, username=cd["username"], password=cd["password"])
-
-#             if user and user.is_active:
-#                 login(request, user)
-#                 return HttpResponseRedirect(reverse("home"))
-    
-#     else:
-#         form = LoginUserForm()
-
-#     data = {
-#         "form": form,
-#     }
-#     return render(request, "users/login.html", context=data)
 
 class LoginUser(LoginView):
     form_class = LoginUserForm
@@ -35,8 +15,26 @@ class LoginUser(LoginView):
 
     def get_success_url(self):
         return reverse_lazy("home")
+    
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password1"])
+            user.save()
+            return render(request, "users/register_done.html")
+    else:      
+        form = RegisterUserForm()
+
+    data = {
+        "form": form,
+    }
+    
+    return render(request, "users/registration.html", context=data)
 
 
-# def logout_user(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse("users:login"))
+
+
+
