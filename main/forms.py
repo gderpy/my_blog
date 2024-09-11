44803f81078feb0537ab0764c1731b4b23b09b7d
Yury Petrov <gderpov@yandex.ru>
2024-09-11 19:20:33 +0500

    Добавлен текстовый редактор (trix-editor)
    Для стилизации пришлось залезать в недры JS, файл main/static/main/js/trix_customizations.js. Не обошлось без ChatGPT
    
    1) Добавлен перевод редактора
    2) Добавил свой placeholder, потому что редактор изначально ничего под собой подобного не подразумевал
    3) Немного откорректировал отступы

--- main/forms.py
+++ main/forms.py
@@ -5,8 +5,12 @@ from .models import Article, Category
 class AddPostForm(forms.ModelForm):
     title = forms.CharField(max_length=255, label="Название статьи", 
                             widget=forms.TextInput(attrs={"class": "form-control mt-2", "placeholder": "Укажите название статьи", "id": "article-title"}))
-    content = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control mt-2", "rows": 3, "id": "exampleFormControlTextarea1"}), 
-                              required=True, label="Содержание статьи")
+    
+    # content = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control mt-2", "rows": 3, "id": "exampleFormControlTextarea1"}), 
+    #                           required=True, label="Содержание статьи")
+    
+    content = forms.CharField(widget=forms.HiddenInput(), label="Текст поля")
+
     category = forms.ModelChoiceField(
         queryset=Category.objects.all(),
         label="Выберите категорию",
